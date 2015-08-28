d52019fcacfb65b6c804e7874b6d18613d975234
David Thompson <dtt101@gmail.com>
2015-08-28 17:05:00 +0100

    WIP: stubbing out tests

---
+++ integration/api/issues/all.spec.js
@@ -0,0 +1,11 @@
+import assert from 'assert';
+import sinon from 'sinon';
+import agent from 'supertest-koa-agent';
+
+describe('INTEGRATION api', () => {
+
+  describe('GET /api/issues', () => {
+
+  });
+
+});
